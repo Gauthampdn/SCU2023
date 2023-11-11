d6c2f629804ac87cbbf039e9deff9da8f9b595fa
Gautham P <gauthampdn@gmail.com>
2023-11-11 15:27:52 -0800

    try

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,10 +1,10 @@
 
 const requireAuth = async (req, res, next) => {
   if (req.isAuthenticated()) {
-    console.log("user is authenticated" + req.user)
+    console.log("user is authenticated " + req.user)
     next()
   } else {
-    console.log("user not authenticated" + req.user)
+    console.log("user not authenticated " + req.user)
     res.status(401).json({ error: "Unauthorized access to person" });
   }
 }
