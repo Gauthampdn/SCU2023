1ec1d9ff57753f5cf079ea7239e3cbee7dbea049
Gautham P <gauthampdn@gmail.com>
2023-11-11 21:34:23 -0800

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,110 +1,23 @@
-import React, { useState, useRef, useMemo } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import './App.css'
-import pinLocation from 'Pin-location.png';
+// src/App.js
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Driver from './components/Driver'; // Import Drive component
+import Passenger from './components/Passenger'; // Import Passenger component
+import Login from './components/Login';
 
-const defaultCenter = { lat: 37.34, lng: -121.938130 };
-
-const startIcon = new L.Icon({
-  iconUrl: pinLocation, // Replace with your start point icon URL
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-});
-
-const endIcon = new L.Icon({
-  iconUrl: pinLocation, // Replace with your end point icon URL
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-});
-const DraggableMarker = ({ content, initialPosition, onDrag }) => {
-  const [position, setPosition] = useState(initialPosition);
-
-  const eventHandlers = useMemo(
-    () => ({
-      dragend() {
-        const marker = ref.current;
-        if (marker != null) {
-          const newPos = marker.getLatLng();
-          setPosition(newPos);
-          onDrag(newPos);
-        }
-      },
-    }),
-    [onDrag],
-  );
-
-  const ref = useRef(null);
-
-  // Only render the Marker if position is not null
-  return position ? (
-    <Marker
-      draggable={true}
-      eventHandlers={eventHandlers}
-      position={position}
-      ref={ref}>
-      <Popup>{content}</Popup>
-    </Marker>
-  ) : null;
-};
-
-const App = () => {
-  const [startPosition, setStartPosition] = useState(defaultCenter);
-const [endPosition, setEndPosition] = useState(defaultCenter);
-  const [isSettingEndPoint, setIsSettingEndPoint] = useState(false);
-  const [showSidebar, setShowSidebar] = useState(false);
-
-  const toggleSidebar = () => {
-    if (endPosition) {
-      setShowSidebar(!showSidebar);
-    }
-  };
-  const updateStartPosition = (newPosition) => {
-    setStartPosition(newPosition);
-  };
-
-  const updateEndPosition = (newPosition) => {
-    setEndPosition(newPosition);
-  };
+function App() {
   return (
-    <div className='Driver'>
-      <div className="instructions">
-        {isSettingEndPoint ? 'Pick the End Point' : 'Pick the Start Point'}
-      </div>
-      <MapContainer center={[37.34, -121.938130]} zoom={13} style={{ height: '100vh', width: '100vw' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <DraggableMarker
-          content="Start Point"
-          initialPosition={startPosition}
-          onDrag={updateStartPosition}
-          icon={startIcon}
-        />
-        <DraggableMarker
-          content="End Point"
-          initialPosition={endPosition}
-          onDrag={updateEndPosition}
-          icon={endIcon}
-        />
-      </MapContainer>
-
-      {showSidebar && (
-        <div className="sidebar">
-          <p>Start Point: {JSON.stringify(startPosition)}</p>
-          <p>End Point: {JSON.stringify(endPosition)}</p>
-          <button onClick={toggleSidebar}>Close Sidebar</button>
-          <button>Driver</button>
-          <button>Passenger</button>
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/drive" component={Driver} />
+        <Route path="/passenger" component={Passenger} />
+        <Route path="/login"component={Login}/>
+      </Switch>
+    </Router>
   );
-};
+}
 
 export default App;
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,23 +1,110 @@
-// src/HomePage.js
+import React, { useState, useRef, useMemo } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import './HomePage.css'
+import pinLocation from 'Pin-location.png';
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import NavBar from './NavBar';
-import './HomePage.css';
 
-function HomePage() {
+const defaultCenter = { lat: 37.34, lng: -121.938130 };
+
+const startIcon = new L.Icon({
+  iconUrl: "pinLocation", // Replace with your start point icon URL
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
+
+const endIcon = new L.Icon({
+  iconUrl: "pinLocation", // Replace with your end point icon URL
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
+const DraggableMarker = ({ content, initialPosition, onDrag }) => {
+  const [position, setPosition] = useState(initialPosition);
+
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = ref.current;
+        if (marker != null) {
+          const newPos = marker.getLatLng();
+          setPosition(newPos);
+          onDrag(newPos);
+        }
+      },
+    }),
+    [onDrag],
+  );
+
+  const ref = useRef(null);
+
+  // Only render the Marker if position is not null
+  return position ? (
+    <Marker
+      draggable={true}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={ref}>
+      <Popup>{content}</Popup>
+    </Marker>
+  ) : null;
+};
+
+const HomePage = () => {
+  const [startPosition, setStartPosition] = useState(defaultCenter);
+const [endPosition, setEndPosition] = useState(defaultCenter);
+  const [isSettingEndPoint, setIsSettingEndPoint] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    if (endPosition) {
+      setShowSidebar(!showSidebar);
+    }
+  };
+  const updateStartPosition = (newPosition) => {
+    setStartPosition(newPosition);
+  };
+
+  const updateEndPosition = (newPosition) => {
+    setEndPosition(newPosition);
+  };
   return (
-    <div>
-      <NavBar />
-      <div className="image-overlay-container">
-        <img src="/blog-hero.jpg" alt="A descriptive text for the image" className="full-width-image" />
-        <div className="button-group">
-          <Link to="/drive" className="action-button">For Drivers</Link>
-          <Link to="/passenger" className="action-button">For Passengers</Link>
-        </div>
+    <div className='Driver'>
+      <div className="instructions">
+        {isSettingEndPoint ? 'Pick the End Point' : 'Pick the Start Point'}
       </div>
+      <MapContainer center={[37.34, -121.938130]} zoom={13} style={{ height: '100vh', width: '100vw' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <DraggableMarker
+          content="Start Point"
+          initialPosition={startPosition}
+          onDrag={updateStartPosition}
+          icon={startIcon}
+        />
+        <DraggableMarker
+          content="End Point"
+          initialPosition={endPosition}
+          onDrag={updateEndPosition}
+          icon={endIcon}
+        />
+      </MapContainer>
+
+      {showSidebar && (
+        <div className="sidebar">
+          <p>Start Point: {JSON.stringify(startPosition)}</p>
+          <p>End Point: {JSON.stringify(endPosition)}</p>
+          <button onClick={toggleSidebar}>Close Sidebar</button>
+          <button>Driver</button>
+          <button>Passenger</button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default HomePage;
