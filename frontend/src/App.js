cdb3ede326ffe7aaae53f059832ebdf113dc64c1
ashw24 <ashchembu@gmail.com>
2023-11-11 21:18:46 -0800

    aszadsdf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,106 @@
-// src/App.js
+import React, { useState, useRef, useMemo } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import './App.css'
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { useAuthContext } from './hooks/useAuthContext'; // Import the authentication context hook
+const startIcon = new L.Icon({
+  iconUrl: 'start-icon.png', // Replace with your start point icon URL
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
 
-// Import your page components
-import HomePage from './components/HomePage';
-import Driver from './components/Driver';
-import Passenger from './components/Passenger';
-import Login from './components/Login';
+const endIcon = new L.Icon({
+  iconUrl: 'end-icon.png', // Replace with your end point icon URL
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
+const DraggableMarker = ({ content, initialPosition, onDrag }) => {
+  const [position, setPosition] = useState(initialPosition);
 
-function App() {
-  const { user } = useAuthContext(); // Destructure user from the auth context
-
-  return (
-    <Router>
-      <Switch>
-        {/* Redirect unauthenticated users to the Login page */}
-        <Route path="/" exact render={() => (
-          user ? <HomePage /> : <Redirect to="/login" />
-        )} />
-        <Route path="/drive" render={() => (
-          user ? <Driver /> : <Redirect to="/login" />
-        )} />
-        <Route path="/passenger" render={() => (
-          user ? <Passenger /> : <Redirect to="/login" />
-        )} />
-        <Route path="/login" render={() => (
-          !user ? <Login /> : <Redirect to="/" />
-        )} />
-      </Switch>
-    </Router>
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = ref.current;
+        if (marker != null) {
+          const newPos = marker.getLatLng();
+          setPosition(newPos);
+          onDrag(newPos);
+        }
+      },
+    }),
+    [onDrag],
   );
-}
+
+  const ref = useRef(null);
+
+  // Only render the Marker if position is not null
+  return position ? (
+    <Marker
+      draggable={true}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={ref}>
+      <Popup>{content}</Popup>
+    </Marker>
+  ) : null;
+};
+
+const App = () => {
+  const [startPosition, setStartPosition] = useState(null);
+  const [endPosition, setEndPosition] = useState(null);
+  const [isSettingEndPoint, setIsSettingEndPoint] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    if (endPosition) {
+      setShowSidebar(!showSidebar);
+    }
+  };
+  const updateStartPosition = (newPosition) => {
+    setStartPosition(newPosition);
+  };
+
+  const updateEndPosition = (newPosition) => {
+    setEndPosition(newPosition);
+  };
+  return (
+    <div className='Driver'>
+      <div className="instructions">
+        {isSettingEndPoint ? 'Pick the End Point' : 'Pick the Start Point'}
+      </div>
+      <MapContainer center={[37.34, -121.938130]} zoom={13} style={{ height: '100vh', width: '100vw' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <DraggableMarker
+          content="Start Point"
+          initialPosition={startPosition}
+          onDrag={updateStartPosition}
+          icon={startIcon}
+        />
+        <DraggableMarker
+          content="End Point"
+          initialPosition={endPosition}
+          onDrag={updateEndPosition}
+          icon={endIcon}
+        />
+      </MapContainer>
+
+      {showSidebar && (
+        <div className="sidebar">
+          <p>Start Point: {JSON.stringify(startPosition)}</p>
+          <p>End Point: {JSON.stringify(endPosition)}</p>
+          <button onClick={toggleSidebar}>Close Sidebar</button>
+          <button>Driver</button>
+          <button>Passenger</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
